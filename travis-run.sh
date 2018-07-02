6ede166a2ce2f571cee070aad0caa9d02ef09e2c
anykao <600913@gmail.com>
2018-07-03 00:37:04 +0900

    Update travis-run.sh

--- travis-run.sh
+++ travis-run.sh
@@ -10,5 +10,5 @@ cd ios
 ./compile-ffmpeg.sh clean
 ./compile-openssl.sh arm64
 ./compile-ffmpeg.sh arm64
-xcodebuild build -project IJKMediaDemo/IJKMediaDemo.xcodeproj -scheme IJKMediaFramework -configuration Release -sdk iphoneos VALID_ARCHS=arm64 CONFIGURATION_BUILD_DIR=.
+xcodebuild build -project IJKMediaDemo/IJKMediaDemo.xcodeproj -scheme IJKMediaFramework -configuration Release -sdk iphoneos VALID_ARCHS=arm64 CONFIGURATION_BUILD_DIR=. | xcpretty
 ls -lh IJKMediaFramework.framework
