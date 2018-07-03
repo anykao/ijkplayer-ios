d71e027bc093b9792b7c28f165e59f181bd54671
anykao <600913@gmail.com>
2018-07-03 20:21:13 +0900

    Update travis-run.sh

--- travis-run.sh
+++ travis-run.sh
@@ -3,10 +3,10 @@
 # Exit on errors.
 set -e
 
-./init-ios-openssl.sh arm64
-./init-ios.sh arm64
+./init-ios-openssl.sh
+./init-ios.sh
 cd ios
 ./compile-openssl.sh clean
 ./compile-ffmpeg.sh clean
-./compile-openssl.sh arm64
-./compile-ffmpeg.sh arm64
+./compile-openssl.sh
+./compile-ffmpeg.sh
